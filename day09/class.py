176ea97a2bf158bd072809b17c0d209123e67eea
PsChina <panshanzxcv.love@163.com>
2019-09-28 23:35:00 +0800

    day10

--- day09/class.py
+++ day09/class.py
@@ -1,2 +1,19 @@
 #coding=utf-8
 
+class Student():
+  def __init__(self,name):
+    self.__name = name
+  @property
+  def name(self):
+    return self.__name
+  @name.setter
+  def name(self, new_name):
+    self.__name = new_name
+
+student = Student('潘杉杉')
+
+print(student.name)
+
+student.name = '潘杉'
+
+print(student.name)
