768873d47414c34cd471217a6e612bbcc15d316a
PsChina <panshanzxcv.love@163.com>
2019-10-02 00:14:33 +0800

    多态

--- day09/class.py
+++ day09/class.py
@@ -106,3 +106,32 @@ print(teacher.name,teacher.age)
 student.learn()
 print(student.name,student.age)
 
+# day13
+
+# 多态
+
+from abc import ABCMeta, abstractmethod
+
+class Pet(object, metaclass=ABCMeta):
+  def __init__(self,nickname):
+    self._nickname = nickname
+  @abstractmethod
+  def make_voice(self):
+    """发出声音"""
+    pass
+
+class Dog(Pet):
+  def make_voice(self):
+    print("%s汪汪汪" % self._nickname)
+
+class Cat(Pet):
+  def make_voice(self):
+    print("%s喵喵喵" % self._nickname)
+
+def main():
+  pets = [Dog('旺财'),Cat('凯蒂')]
+  for pet in pets:
+    pet.make_voice()
+
+if __name__ == '__main__':
+  main()
