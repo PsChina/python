f1a3230202818b00b55e8d12ad4cc86ae7899f35
PsChina <panshanzxcv.love@163.com>
2019-10-10 23:59:06 +0800

    write file

---
+++ day11/a.js
@@ -0,0 +1,1 @@
+const a = 10; console.log(a)
--- day11/file.py
+++ day11/file.py
@@ -12,6 +12,10 @@ def print_text_line_by_line():
         print("%d:%s" % (line_index, each_line))
         line_index += 1
 
+def write_file():
+        open('a.js','w', encoding='utf-8').write(str('const a = 10; console.log(a)'))
+
 if __name__ == '__main__':
     # main()
-    print_text_line_by_line()
+    # print_text_line_by_line()
+    write_file()
